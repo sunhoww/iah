c530c46c21ec2a609ad335c1f4c45ffee33b46ae
Sun Howwrongbum <sun@libermatic.com>
2022-11-12 09:12:13 +0000

    fix: rm `ref_patient_appointment`

--- iah/patches/v14_0/setup_patient_appointment.py
+++ iah/patches/v14_0/setup_patient_appointment.py
@@ -3,24 +3,7 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_field
 
 
 def execute():
-    _make_custom_fields()
     _make_property_setters()
-
-
-def _make_custom_fields():
-    docfields = [
-        {
-            "label": "Ref Patient Appointment",
-            "fieldname": "ref_patient_appointment",
-            "fieldtype": "Link",
-            "options": "Patient Appointment",
-            "set_only_once": 1,
-            "no_copy": 1,
-            "insert_after": "status",
-        }
-    ]
-    for df in docfields:
-        create_custom_field("Patient Appointment", df)
 
 
 def _make_property_setters():
