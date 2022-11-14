a6507522db221dde3a841f407b1aea0d035fab8c
Sun Howwrongbum <sun@libermatic.com>
2022-11-14 19:16:42 +0000

    feat: do not update **PatientAppointment** from **TherapySession**

--- iah/overrides/therapy_session.py
+++ iah/overrides/therapy_session.py
@@ -4,6 +4,12 @@ from healthcare.healthcare.doctype.therapy_session.therapy_session import (
 
 
 class TherapySession(Standard):
+    def on_submit(self):
+        pass
+
+    def on_cancel(self):
+        pass
+
     def update_sessions_count_in_therapy_plan(self, on_cancel=False):
         if self.therapy_plan:
             super().update_sessions_count_in_therapy_plan(on_cancel=on_cancel)
