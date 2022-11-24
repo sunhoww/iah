0f7c7d7157d44a1139e5445ca7af99f954f7d071
Sun Howwrongbum <sun@libermatic.com>
2022-11-24 19:27:49 +0000

    fix: prevent **PatientAppointment** update from **TherapySession**

--- iah/overrides/therapy_session.py
+++ iah/overrides/therapy_session.py
@@ -4,7 +4,12 @@ from healthcare.healthcare.doctype.therapy_session.therapy_session import (
 
 
 class TherapySession(Standard):
+    # on_submit, on_update, on_cancel are overriden to prevent updating
+    # Patient Appointment status
     def on_submit(self):
+        pass
+
+    def on_update(self):
         pass
 
     def on_cancel(self):
