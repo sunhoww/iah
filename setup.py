6dc5cef3c9bc6b3515cb47c178f8e5636b655d12
Sun Howwrongbum <sun@libermatic.com>
2023-03-23 00:10:29 +0530

    refactor: use pyproject

--- setup.py
+++ setup.py
@@ -1,19 +1,6 @@
+# TODO: Remove this file when bench >=v5.11.0 is adopted / v15.0.0 is released
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-	install_requires = f.read().strip().split("\n")
+name="iah"
 
-# get version from __version__ variable in iah/__init__.py
-from iah import __version__ as version
-
-setup(
-	name="iah",
-	version=version,
-	description="Customizations for IAH",
-	author="Libermatic",
-	author_email="info@libermatic.com",
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
-)
+setup()
